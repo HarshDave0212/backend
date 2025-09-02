d1b68e7b0de76f7d8946d82a1e67a41fa37b0729
HarshDave <harshdave0212@gmail.com>
2025-09-03 00:18:03 +0530

    Fix CORS configuration and update JWT verification parameter

--- src/app.js
+++ src/app.js
@@ -7,13 +7,14 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
+    credentials: true,
   })
 );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded());
 app.use(express.static("public"));
-(app.use(cookieParser()));
+app.use(cookieParser());
 //routes import
 
 import userRouter from "./routes/user.routes.js";
